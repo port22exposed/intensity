c14221c102b0020ad5983b710526bad81cb4b635
port22exposed <scm@port22.exposed>
2025-02-17 04:05:10 +0000

    add error page and fix log var

--- src/main.zig
+++ src/main.zig
@@ -1,15 +1,9 @@
+const builtin = @import("builtin");
 const std = @import("std");
 const zap = @import("zap");
 
 fn on_request(r: zap.Request) void {
-    if (r.path) |the_path| {
-        std.debug.print("PATH: {s}\n", .{the_path});
-    }
-
-    if (r.query) |the_query| {
-        std.debug.print("QUERY: {s}\n", .{the_query});
-    }
-    r.sendBody("<html><body><h1>Hello from ZAP!!!</h1></body></html>") catch return;
+    r.sendBody("<html><body><h1>error 404: not found</h1></body></html>") catch return;
 }
 
 pub fn main() !void {
@@ -17,7 +11,7 @@ pub fn main() !void {
         .port = 3000,
         .on_request = on_request,
         .public_folder = "public",
-        .log = std.builtin.OptimizeMode == .Debug,
+        .log = builtin.mode == .Debug,
     });
     try listener.listen();
 
