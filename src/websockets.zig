12c5080ea786100986f425899b7f13b02e357b39
port22exposed <scm@port22.exposed>
2025-02-17 21:11:49 +0000

    fix the returns

--- src/websockets.zig
+++ src/websockets.zig
@@ -5,13 +5,13 @@ const WebSockets = zap.WebSockets;
 fn deny_request(r: zap.Request) void {
     r.setStatus(.bad_request);
     r.sendBody("400 - BAD REQUEST") catch unreachable;
-    return;
 }
 
 pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
     if (!std.mem.eql(u8, target_protocol, "websocket")) {
         std.log.warn("received illegal protocol: {s}", .{target_protocol});
         deny_request(r);
+        return;
     }
 
     const allocator = std.heap.page_allocator;
@@ -26,10 +26,12 @@ pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
             @memcpy(username.?, name.str);
         } else {
             deny_request(r);
+            return;
         }
     } else |e| {
         std.log.err("failed to get `username` parameter string: {any}", .{e});
         deny_request(r);
+        return;
     }
 
     std.log.info("username: {any}", .{username});
