5a5d57e5fbd80090e838f0624d5a2023d6587fdc
port22exposed <scm@port22.exposed>
2025-02-17 04:16:31 +0000

    autoscroll

--- public/script.js
+++ public/script.js
@@ -3,9 +3,34 @@ function createMessage(user, text) {
 	message.className = "message";
 	message.innerHTML = `<span class="name">${user}</span>${text}`;
 	return message;
-
 }
 
-document.getElementById("send").onclick = () => {
-	
+const messagelist = document.getElementById("messagelist");
+const messagebox = document.getElementById("message");
+const sendbutton = document.getElementById("send");
+
+function send() {
+	messagelist.appendChild(createMessage("User1", messagebox.value));
+	messagebox.value = "";
+	messagelist.scrollTop = messagelist.scrollHeight;
 }
+
+messagebox.addEventListener("keydown", (e) => {
+	if (e.key === "Enter") {
+		send()
+	}
+});
+
+sendbutton.onclick = () => {
+	send()
+}
+
+const observer = new MutationObserver(() => {
+	if (messagelist.scrollTop + messagelist.clientHeight >= messagelist.scrollHeight - messagelist.lastChild.clientHeight*2) {
+		messagelist.scrollTop = messagelist.scrollHeight;
+	}
+});
+
+
+const config = { childList: true };
+observer.observe(messagelist, config);
