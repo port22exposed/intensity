20a9401a11951eac058072892643c48e7fe40c7a
port22exposed <scm@port22.exposed>
2025-02-17 08:14:51 +0000

    impl param grabbing and username detection

--- src/websockets.zig
+++ src/websockets.zig
@@ -9,4 +9,32 @@ pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
         r.sendBody("400 - BAD REQUEST") catch unreachable;
         return;
     }
+
+    var username: ?[]const u8 = undefined;
+
+    if (r.query) |query| {
+        var params = std.mem.splitAny(u8, query, "&");
+        while (params.next()) |param| {
+            var kv = std.mem.split(u8, param, "=");
+            if (kv.next()) |key| {
+                if (kv.next()) |value| {
+                    if (std.mem.eql(u8, key, "username")) {
+                        username = value;
+                    }
+                }
+            }
+        }
+    } else {
+        r.setStatus(.bad_request);
+        r.sendBody("400 - BAD REQUEST") catch unreachable;
+        return;
+    }
+
+    if (username) |name| {
+        std.log.info("username: {s}", .{name});
+    } else {
+        r.setStatus(.bad_request);
+        r.sendBody("400 - BAD REQUEST") catch unreachable;
+        return;
+    }
 }
