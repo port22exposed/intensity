e904dabecf6893a132324759e5a4800f3b9a6368
port22exposed <scm@port22.exposed>
2025-02-17 08:17:18 +0000

    stop rewriting the same deny request code

--- src/websockets.zig
+++ src/websockets.zig
@@ -2,12 +2,16 @@ const std = @import("std");
 const zap = @import("zap");
 const WebSockets = zap.WebSockets;
 
+fn deny_request(r: zap.Request) void {
+    r.setStatus(.bad_request);
+    r.sendBody("400 - BAD REQUEST") catch unreachable;
+    return;
+}
+
 pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
     if (!std.mem.eql(u8, target_protocol, "websocket")) {
         std.log.warn("received illegal protocol: {s}", .{target_protocol});
-        r.setStatus(.bad_request);
-        r.sendBody("400 - BAD REQUEST") catch unreachable;
-        return;
+        deny_request(r);
     }
 
     var username: ?[]const u8 = undefined;
@@ -25,16 +29,12 @@ pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
             }
         }
     } else {
-        r.setStatus(.bad_request);
-        r.sendBody("400 - BAD REQUEST") catch unreachable;
-        return;
+        deny_request(r);
     }
 
     if (username) |name| {
         std.log.info("username: {s}", .{name});
     } else {
-        r.setStatus(.bad_request);
-        r.sendBody("400 - BAD REQUEST") catch unreachable;
-        return;
+        deny_request(r);
     }
 }
