24f3eadb3ad86e58a5106f4d2a88148770f12afc
port22exposed <scm@port22.exposed>
2025-02-17 09:55:28 +0000

    horrifying code I will fix later

--- src/websockets.zig
+++ src/websockets.zig
@@ -14,27 +14,33 @@ pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
         deny_request(r);
     }
 
-    var username: ?[]const u8 = undefined;
+    const allocator = std.heap.page_allocator;
 
-    if (r.query) |query| {
-        var params = std.mem.splitAny(u8, query, "&");
-        while (params.next()) |param| {
-            var kv = std.mem.split(u8, param, "=");
-            if (kv.next()) |key| {
-                if (kv.next()) |value| {
-                    if (std.mem.eql(u8, key, "username")) {
-                        username = value;
-                    }
-                }
-            }
+    r.parseQuery(); // this took too long to find please explode zig zap!
+
+    var username: ?[]u8 = null;
+
+    if (r.getParamStr(allocator, "username", false)) |maybe_username| {
+        if (maybe_username) |*name| {
+            username = allocator.alloc(u8, name.str.len) catch unreachable;
+            @memcpy(username.?, name.str);
+        } else {
+            deny_request(r);
         }
-    } else {
+    } else |e| {
+        std.log.err("failed to get `username` parameter string: {any}", .{e});
         deny_request(r);
     }
 
-    if (username) |name| {
-        std.log.info("username: {s}", .{name});
-    } else {
-        deny_request(r);
-    }
+    std.log.info("username: {any}", .{username});
+
+    // } else {
+    //     deny_request(r);
+    // }
+
+    // if (username) |name| {
+    //     std.log.info("username: {s}", .{name});
+    // } else {
+    //     deny_request(r);
+    // }
 }
