2e464887d424092bf5af958f90d98ed7cd7ed8c0
port22exposed <scm@port22.exposed>
2025-02-18 02:13:01 +0000

    username validation function

--- src/websockets.zig
+++ src/websockets.zig
@@ -7,6 +7,24 @@ fn deny_request(r: zap.Request) void {
     r.sendBody("400 - BAD REQUEST") catch unreachable;
 }
 
+// Usernames must be between 3-20 characters
+// Character set of A-Z, a-z, 0-9 or _ and -
+fn is_valid_user(username: []const u8) bool {
+    var valid = true;
+
+    for (username) |char| {
+        if (!std.ascii.isAlphanumeric(char) and char != '_' and char != '-') {
+            valid = false;
+        }
+    }
+
+    if (username.len < 3 or username.len > 20) {
+        valid = false;
+    }
+
+    return valid;
+}
+
 pub fn on_upgrade(r: zap.Request, target_protocol: []const u8) void {
     if (!std.mem.eql(u8, target_protocol, "websocket")) {
         std.log.warn("received illegal protocol: {s}", .{target_protocol});
