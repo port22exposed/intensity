f9a31d47a849f61807642772c6fc4c39f87c053c
port22exposed <scm@port22.exposed>
2025-02-19 20:42:25 +0000

    move functions that work fine on there own outside of setTimeout

--- public/script.js
+++ public/script.js
@@ -1,19 +1,26 @@
-setTimeout(() => {
-	function is_valid_username (username) {
-		let valid = username != null;
-		const len = username.length;
-	
-		if (len < 3 || len > 20) {
-			valid = false;
-		}
-	
-		if (!(/^[a-zA-Z0-9]+$/.test(username))) {
-			valid = false;
-		}
-		
-		return valid;
+function is_valid_username (username) {
+	let valid = username != null;
+	const len = username.length;
+
+	if (len < 3 || len > 20) {
+		valid = false;
+	}
+
+	if (!(/^[a-zA-Z0-9]+$/.test(username))) {
+		valid = false;
 	}
 	
+	return valid;
+}
+
+function createMessage(user, text) {
+	const message = document.createElement("div");
+	message.className = "message";
+	message.innerHTML = `<span class="name">${user}</span>${text}`;
+	return message;
+}
+
+setTimeout(() => {
 	const username = prompt("Enter a username to join!", "user");
 	
 	if (!is_valid_username(username)) {
@@ -24,22 +31,15 @@ setTimeout(() => {
 	
 	const websocket = new WebSocket(`${window.location.protocol === "https:" ? "wss:" : "ws:"}//${window.location.host}/?username=${username}`);
 	
-	function createMessage(user, text) {
-		const message = document.createElement("div");
-		message.className = "message";
-		message.innerHTML = `<span class="name">${user}</span>${text}`;
-		return message;
-	}
-	
-	const messagelist = document.getElementById("messagelist");
-	const messagebox = document.getElementById("message");
-	const sendbutton = document.getElementById("send");
-	
 	function send() {
 		messagelist.appendChild(createMessage(username, messagebox.value));
 		messagebox.value = "";
 		messagelist.scrollTop = messagelist.scrollHeight;
 	}
+
+	const messagelist = document.getElementById("messagelist");
+	const messagebox = document.getElementById("message");
+	const sendbutton = document.getElementById("send");
 	
 	messagebox.addEventListener("keydown", (e) => {
 		if (e.key === "Enter") {
