4bd8b646ff9af0eec44844f4047c4e4a1359e1d7
port22exposed <scm@port22.exposed>
2025-02-19 20:36:30 +0000

    fixes and make username apply to the field

--- public/script.js
+++ public/script.js
@@ -1,59 +1,63 @@
-function is_valid_username (username) {
-	let valid = username != null;
-	const len = username.length;
-
-	if (len < 3 || len > 20) {
-		valid = false;
-	}
-
-	if (!(/^[a-zA-Z0-9]+$/.test(username))) {
-		valid = false;
+window.onload = setTimeout(() => {
+	function is_valid_username (username) {
+		let valid = username != null;
+		const len = username.length;
+	
+		if (len < 3 || len > 20) {
+			valid = false;
+		}
+	
+		if (!(/^[a-zA-Z0-9]+$/.test(username))) {
+			valid = false;
+		}
+		
+		return valid;
 	}
 	
-	return valid;
-}
-
-const username = prompt("Enter a username to join!", "user");
-
-if (!is_valid_username(username)) {
-	window.location.reload();
-}
-
-const websocket = new WebSocket(`${window.location.protocol === "https:" ? "wss:" : "ws:"}//${window.location.host}/?username=${username}`);
-
-function createMessage(user, text) {
-	const message = document.createElement("div");
-	message.className = "message";
-	message.innerHTML = `<span class="name">${user}</span>${text}`;
-	return message;
-}
-
-const messagelist = document.getElementById("messagelist");
-const messagebox = document.getElementById("message");
-const sendbutton = document.getElementById("send");
-
-function send() {
-	messagelist.appendChild(createMessage("User1", messagebox.value));
-	messagebox.value = "";
-	messagelist.scrollTop = messagelist.scrollHeight;
-}
-
-messagebox.addEventListener("keydown", (e) => {
-	if (e.key === "Enter") {
-		send()
+	const username = prompt("Enter a username to join!", "user");
+	
+	if (!is_valid_username(username)) {
+		window.location.reload();
 	}
-});
-
-sendbutton.onclick = () => {
-	send()
-}
-
-const observer = new MutationObserver(() => {
-	if (messagelist.scrollTop + messagelist.clientHeight >= messagelist.scrollHeight - messagelist.lastChild.clientHeight*2) {
+	
+	document.getElementById("name").innerText = username;
+	
+	const websocket = new WebSocket(`${window.location.protocol === "https:" ? "wss:" : "ws:"}//${window.location.host}/?username=${username}`);
+	
+	function createMessage(user, text) {
+		const message = document.createElement("div");
+		message.className = "message";
+		message.innerHTML = `<span class="name">${user}</span>${text}`;
+		return message;
+	}
+	
+	const messagelist = document.getElementById("messagelist");
+	const messagebox = document.getElementById("message");
+	const sendbutton = document.getElementById("send");
+	
+	function send() {
+		messagelist.appendChild(createMessage(username, messagebox.value));
+		messagebox.value = "";
 		messagelist.scrollTop = messagelist.scrollHeight;
 	}
-});
-
-
-const config = { childList: true };
-observer.observe(messagelist, config);
+	
+	messagebox.addEventListener("keydown", (e) => {
+		if (e.key === "Enter") {
+			send()
+		}
+	});
+	
+	sendbutton.onclick = () => {
+		send()
+	}
+	
+	const observer = new MutationObserver(() => {
+		if (messagelist.scrollTop + messagelist.clientHeight >= messagelist.scrollHeight - messagelist.lastChild.clientHeight*2) {
+			messagelist.scrollTop = messagelist.scrollHeight;
+		}
+	});
+	
+	
+	const config = { childList: true };
+	observer.observe(messagelist, config);
+}, 100)
