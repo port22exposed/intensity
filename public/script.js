e788bccad70fc167e743ac487d2dcb42e0eac16d
zramctl <zerowatts@tuta.io>
2025-02-20 14:13:59 -0500

    refactor: :zap: use good practices

--- public/script.js
+++ public/script.js
@@ -1,97 +1,100 @@
 function exit() {
-	window.location.href = "about:blank"
+	window.location.href = "about:blank";
 }
 
 function is_valid_username(username) {
-	let valid = username != null
-	const len = username.length
+	let valid = username != null;
+	const len = username.length;
 
 	if (len < 3 || len > 20) {
-		valid = false
+		valid = false;
 	}
 
 	if (!/^[a-zA-Z0-9]+$/.test(username)) {
-		valid = false
+		valid = false;
 	}
 
-	return valid
+	return valid;
 }
 
 function createMessage(user, text, className) {
-	const message = document.createElement("div")
-	message.className = className || "message"
-	message.innerHTML = `<span class="name">${user}</span>${text}`
-	return message
+	const message = document.createElement("div");
+	message.className = className || "message";
+	message.innerHTML = `<span class="name">${user}</span>${text}`;
+	return message;
 }
 
 function createSystemMessage(text) {
-	const message = document.createElement("div")
-	message.className = "message info"
-	message.innerText = text
-	return message
+	const message = document.createElement("div");
+	message.className = "message info";
+	message.innerText = text;
+	return message;
 }
 
 window.onload = () => {
-	const username = prompt("Enter a username to join!\n\n[WARNING]: The username is shared with the server unencrypted!", "user")
+	const username = prompt(
+		"Enter a username to join!\n\n[WARNING]: The username is shared with the server unencrypted!",
+		"user",
+	);
 
 	if (!is_valid_username(username)) {
-		window.location.reload()
+		window.location.reload();
 	}
 
-	document.getElementById("name").innerText = username
+	document.getElementById("name").innerText = username;
 
-	var websocket = new WebSocket(
+	const websocket = new WebSocket(
 		`${window.location.protocol === "https:" ? "wss:" : "ws:"}//${
 			window.location.host
-		}/?username=${username}`
-	)
+		}/?username=${username}`,
+	);
 
-	websocket.onerror = exit
-	websocket.onclose = exit
+	websocket.onerror = exit;
+	websocket.onclose = exit;
 
-	const usercount = document.getElementById("usercount")
-	const messagelist = document.getElementById("messagelist")
-	const messagebox = document.getElementById("message")
-	const sendbutton = document.getElementById("send")
+	const usercount = document.getElementById("usercount");
+	const messagelist = document.getElementById("messagelist");
+	const messagebox = document.getElementById("message");
+	const sendbutton = document.getElementById("send");
 
-	function updateScrollPosition () {
+	function updateScrollPosition() {
 		if (
 			messagelist.scrollTop + messagelist.clientHeight >=
 			messagelist.scrollHeight - messagelist.lastChild.clientHeight * 2
 		) {
-			messagelist.scrollTop = messagelist.scrollHeight
+			messagelist.scrollTop = messagelist.scrollHeight;
 		}
 	}
 
 	function sendSystem(text) {
-		messagelist.appendChild(createSystemMessage(text))
-		updateScrollPosition()
+		messagelist.appendChild(createSystemMessage(text));
+		updateScrollPosition();
 	}
 
-	sendSystem("Type `/help` for commands!")
+	sendSystem("Type `/help` for commands!");
 
-	websocket.onmessage = function (e) {
-		const packet = JSON.parse(e.data)
-		if (packet.type == "update") {
-			const data = packet.data
+	websocket.onmessage = (e) => {
+		const packet = JSON.parse(e.data);
+		if (packet.type === "update") {
+			const data = packet.data;
 			if (data.userCount) {
-				usercount.innerText = data.userCount
+				usercount.innerText = data.userCount;
 			}
 			if (data.userJoining) {
-				sendSystem(`${data.userJoining} has joined the chat.`)
+				sendSystem(`${data.userJoining} has joined the chat.`);
 			}
 			if (data.userLeaving) {
-				sendSystem(`${data.userLeaving} has left the chat.`)
+				sendSystem(`${data.userLeaving} has left the chat.`);
 			}
 		}
-	}
+	};
 
 	function send() {
-		const message = messagebox.value
+		const message = messagebox.value;
 		if (message.startsWith("/")) {
-			const args = message.split(" ")
-			const command = args[0].substring(1)
-			if (command == "help") {
+			const args = message.split(" ");
+			const command = args[0].substring(1);
+			if (command === "help") {
 				sendSystem(`Intensity Chat - Commands List
 
 EVERYONE:
@@ -108,21 +111,21 @@ OPERATOR:
 OWNER:
 
 /op <username> - gives the user operator status
-/transfer <username> - transfers ownership of the group chat to another member`)
+/transfer <username> - transfers ownership of the group chat to another member`);
 			}
 		} else {
-			messagelist.appendChild(createMessage(username, messagebox.value))
-			messagelist.scrollTop = messagelist.scrollHeight
-			updateScrollPosition()
+			messagelist.appendChild(createMessage(username, messagebox.value));
+			messagelist.scrollTop = messagelist.scrollHeight;
+			updateScrollPosition();
 		}
-		messagebox.value = ""
+		messagebox.value = "";
 	}
 
 	messagebox.addEventListener("keydown", (e) => {
 		if (e.key === "Enter") {
-			send()
+			send();
 		}
-	})
+	});
 
 	sendbutton.onclick = send;
-}
+};
