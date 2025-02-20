a79b3395d5277f6aa8c008d5ed9b47b8240484ef
zramctl <zerowatts@tuta.io>
2025-02-20 13:58:28 -0500

    feat: :sparkles: add nix flake

---
+++ flake.nix
@@ -0,0 +1,24 @@
+{
+  description = "intensity flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          system = system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell
+          {
+            nativeBuildInputs = with pkgs; [
+              zig
+            ];
+          };
+      });
+}
