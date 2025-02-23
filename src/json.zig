e820648467599681928d904475aa4e69fd094534
port22exposed <scm@port22.exposed>
2025-02-23 00:09:26 +0000

    JSON getValue

---
+++ src/json.zig
@@ -0,0 +1,13 @@
+const std = @import("std");
+
+pub fn getValue(comptime T: type, object: std.json.ObjectMap, key: []const u8) !T {
+    const value = object.get(key) orelse return error.KeyNotFound;
+    return switch (value) {
+        .integer => |i| if (T == i64) i else error.TypeMismatch,
+        .float => |f| if (T == f64) f else error.TypeMismatch,
+        .string => |s| if (T == []const u8) s else error.TypeMismatch,
+        .bool => |b| if (T == bool) b else error.TypeMismatch,
+        .object => |o| if (T == std.json.ObjectMap) o else error.TypeMismatch,
+        else => error.TypeMismatch,
+    };
+}
