95b779b1311205375b7304fa780b288b628bc08e
port22exposed <scm@port22.exposed>
2025-02-24 02:47:02 +0000

    allow the user to specify the amount of threads and workers

--- src/main.zig
+++ src/main.zig
@@ -64,6 +64,7 @@ const WebSocketHandler = ws.WebSocketHandler;
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{
         .thread_safe = true,
+        .stack_trace_frames = 32,
     }){};
     const allocator = gpa.allocator();
     defer {
@@ -82,6 +83,8 @@ pub fn main() !void {
 
     var port: usize = 3000;
     var frontendDirectory: []const u8 = "public";
+    var threads: u8 = 2;
+    var workers: u8 = 2;
 
     while (args_it.next()) |arg| {
         if (std.mem.startsWith(u8, arg, "--port=")) {
@@ -95,6 +98,22 @@ pub fn main() !void {
 
         if (std.mem.startsWith(u8, arg, "--frontend=")) {
             frontendDirectory = arg[11..];
+        }
+
+        if (std.mem.startsWith(u8, arg, "--threads=")) {
+            if (std.fmt.parseUnsigned(u8, arg[10..], 0)) |the_threads| {
+                threads = the_threads;
+            } else |_| {
+                std.log.warn("Invalid thread count (range of 1-255). Using default of {}\n", .{threads});
+            }
+        }
+
+        if (std.mem.startsWith(u8, arg, "--workers=")) {
+            if (std.fmt.parseUnsigned(u8, arg[10..], 0)) |the_workers| {
+                workers = the_workers;
+            } else |_| {
+                std.log.warn("Invalid worker count (range of 1-255). Using default of {}\n", .{workers});
+            }
         }
     }
 
@@ -115,7 +134,7 @@ pub fn main() !void {
     std.log.info("Terminate with CTRL+C", .{});
 
     zap.start(.{
-        .threads = @intCast(try std.Thread.getCpuCount()),
-        .workers = 4,
+        .threads = threads,
+        .workers = workers,
     });
 }
