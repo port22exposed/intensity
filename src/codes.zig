6dd6342559a0922ce65da57a02e240a60beb1d83
port22exposed <scm@port22.exposed>
2025-03-16 08:47:52 +0000

    better test logic

--- src/codes.zig
+++ src/codes.zig
@@ -9,7 +9,7 @@ pub const JoinCode = struct {
 
     const Self = @This();
 
-    pub fn parse(allocator: std.mem.Allocator, code: []const u8) !Self {
+    pub fn parse(allocator: std.mem.Allocator, code: []u8) !Self {
         const first_sep_index = std.mem.indexOf(u8, code, "|") orelse {
             return error.InvalidCode;
         };
@@ -26,14 +26,22 @@ pub const JoinCode = struct {
         return Self{ .allocator = allocator, .code = code, .timestamp = try std.fmt.parseInt(i64, id, 10), .username = username, .randomComponent = random_component };
     }
 
-    pub fn deinit(self: *Self) void {
+    pub fn deinit(self: Self) void {
         self.allocator.free(self.code);
     }
 };
 
 test "JoinCode tests" {
-    const parsed = try JoinCode.parse(std.testing.allocator, "1234|username|randomcodethatcancontain|the|separator");
-    try std.testing.expectEqual(1234, parsed.timestamp);
-    try std.testing.expectEqualStrings("username", parsed.username);
-    try std.testing.expectEqualStrings("randomcodethatcancontain|the|separator", parsed.randomComponent);
+    const timestamp: i64 = 1234;
+    const username: []const u8 = "username";
+    const random_component: []const u8 = "randomcodethatcancontain|the|separator";
+
+    const code = try std.fmt.allocPrint(std.testing.allocator, "{d}|{s}|{s}", .{ timestamp, username, random_component });
+
+    const parsed = try JoinCode.parse(std.testing.allocator, code);
+    defer parsed.deinit();
+
+    try std.testing.expectEqual(timestamp, parsed.timestamp);
+    try std.testing.expectEqualStrings(username, parsed.username);
+    try std.testing.expectEqualStrings(random_component, parsed.randomComponent);
 }
