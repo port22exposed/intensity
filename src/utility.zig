b4bd36c53ab8e991895491c827a7fcd1128b036d
port22exposed <scm@port22.exposed>
2025-03-16 02:33:58 +0000

    document the function

--- src/utility.zig
+++ src/utility.zig
@@ -2,6 +2,8 @@ const std = @import("std");
 
 const rand = std.crypto.random;
 
+/// Generates a random string in the form of a u8 byte array.
+/// The caller must free the memory!
 pub fn randomString(allocator: std.mem.Allocator, length: usize) ![]u8 {
     var result = try std.ArrayList(u8).initCapacity(allocator, length);
     errdefer result.deinit();
