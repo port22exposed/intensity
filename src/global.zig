e3b0d614580ab20f993a334acf7ab5da9eef9c29
port22exposed <scm@port22.exposed>
2025-03-16 01:55:38 +0000

    create basic function exporter

---
+++ src/global.zig
@@ -0,0 +1,14 @@
+const std = @import("std");
+
+const State = @import("./state.zig").State;
+
+var global_state: State = undefined;
+
+pub fn initState(allocator: std.mem.Allocator) *State {
+    global_state = State.init(allocator);
+    return &global_state;
+}
+
+pub fn getState() *State {
+    return &global_state;
+}
