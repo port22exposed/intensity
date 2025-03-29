ab5c18f3ea3adda546e715797ff128c89e70c634
port22exposed <scm@port22.exposed>
2025-03-29 16:18:18 +0000

    implement join code handling within upgrade handler

--- src/listener/upgrade/internal.zig
+++ src/listener/upgrade/internal.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const zap = @import("zap");
 
+const codes = @import("../../codes.zig");
 const global = @import("../../global.zig");
 const utility = @import("../../utility.zig");
 
@@ -16,11 +17,34 @@ pub fn internal_upgrade(r: zap.Request, target_protocol: []const u8) UpgradeErro
         return error.IllegalProtocol;
     }
 
-    var context = context_manager.newContext() catch {
-        return error.ContextCreationException;
+    const provided_join_code = r.getParamSlice("auth") orelse {
+        return error.IllegalRequest;
     };
 
-    WebSocketHandler.upgrade(r.h, &context.settings) catch {
-        return error.WebSocketHandlerUpgradeFail;
-    };
+    const state = global.getState();
+    state.mutex.lock();
+    defer state.mutex.unlock();
+
+    var questionable_join_code_struct: ?codes.JoinCode = null;
+
+    for (state.join_codes.items, 0..) |join_code, index| {
+        if (std.mem.eql(u8, provided_join_code, join_code.code)) {
+            questionable_join_code_struct = state.join_codes.swapRemove(index);
+            break;
+        }
+    }
+
+    if (questionable_join_code_struct) |join_code_struct| {
+        defer join_code_struct.deinit();
+
+        var context = context_manager.newContext(join_code_struct.username) catch {
+            return error.ContextCreationException;
+        };
+
+        WebSocketHandler.upgrade(r.h, &context.settings) catch {
+            return error.WebSocketHandlerUpgradeFail;
+        };
+    } else {
+        return error.IllegalRequest;
+    }
 }
