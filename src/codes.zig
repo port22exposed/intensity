59e02d9223ff499294772ecfa233a9897ae43c2f
port22exposed <scm@port22.exposed>
2025-03-29 15:43:01 +0000

    remove unneeded variables, the username doesn't need to be inside of the join code

--- src/codes.zig
+++ src/codes.zig
@@ -3,40 +3,22 @@ const std = @import("std");
 const utility = @import("./utility.zig");
 
 pub const RANDOM_COMPONENT_LENGTH = 128;
-pub const MAX_USERNAME_LENGTH = 20;
-pub const MAX_JOIN_CODE_LENGTH = MAX_USERNAME_LENGTH + RANDOM_COMPONENT_LENGTH + 1; // Accounts for the username and random_component length + 1 for the separator character.
 
 pub const JoinCode = struct {
     allocator: std.mem.Allocator,
     code: []u8,
     username: []u8,
-    random_component: []u8,
 
     const Self = @This();
 
     pub fn init(allocator: std.mem.Allocator, username: []const u8) !Self {
-        const code = try allocator.alloc(u8, username.len + 1 + RANDOM_COMPONENT_LENGTH);
-
-        var index: usize = 0;
-
-        std.mem.copyForwards(u8, code[index..][0..username.len], username);
-        const username_slice = code[index..][0..username.len];
-        index += username.len;
-
-        code[index] = '|';
-        index += 1;
-
-        const random_component = try utility.randomString(allocator, RANDOM_COMPONENT_LENGTH);
-        defer allocator.free(random_component);
-
-        std.mem.copyForwards(u8, code[index..][0..RANDOM_COMPONENT_LENGTH], random_component);
-        const random_component_slice = code[index..][0..RANDOM_COMPONENT_LENGTH];
+        const owned_username = try allocator.dupe(u8, username);
+        const code = try utility.randomString(allocator, RANDOM_COMPONENT_LENGTH);
 
         return Self{
             .allocator = allocator,
             .code = code,
-            .username = username_slice,
-            .random_component = random_component_slice,
+            .username = owned_username,
         };
     }
 
