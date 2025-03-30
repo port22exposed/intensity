cfb462307f9cb4ea6ecc14f810504cdedb2c1721
port22exposed <scm@port22.exposed>
2025-03-31 00:12:41 +0100

    UPDATE packet

--- public/packets.js
+++ public/packets.js
@@ -9,13 +9,14 @@ export function onmessage(e) {
 		if (data.username) {
 			dom.clientUsername.innerText = data.username
 		}
-		// if (data.userCount) {
-		// 	if (data.userCount == "1") {
-		// 		dom.usercount.innerText = `${data.userCount} user`
-		// 	} else {
-		// 		dom.usercount.innerText = `${data.userCount} users`
-		// 	}
-		// }
+	} else if (packet.type == "update") {
+		if (data.userCount) {
+			if (data.userCount == "1") {
+				dom.usercount.innerText = `${data.userCount} user`
+			} else {
+				dom.usercount.innerText = `${data.userCount} users`
+			}
+		}
 	} else if (packet.type == "systemMessage") {
 		sendSystem(data.message)
 	}
