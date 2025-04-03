48d8fa847a546572936c80ffcbb492c6cd781b91
port22exposed <scm@port22.exposed>
2025-04-03 04:42:22 +0100

    add imports

--- src/websockets/on_close.zig
+++ src/websockets/on_close.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+
+const global = @import("../global.zig");
 const context_manager = @import("./context_manager.zig");
 
 pub fn handler(context: ?*context_manager.Context, uuid: isize) void {
