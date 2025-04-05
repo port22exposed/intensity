c40c9104ba85dda33520398ca794111fba9b161f
port22exposed <scm@port22.exposed>
2025-04-05 23:49:19 +0100

    handle message function

--- src/websockets/handle_message.zig
+++ src/websockets/handle_message.zig
@@ -1,7 +1,12 @@
+const std = @import("std");
+
 const zap = @import("zap");
 const WebSockets = zap.WebSockets;
 
+const allocator = @import("../main.zig").allocator;
+
 const context_manager = @import("./context_manager.zig");
+const json = @import("./json.zig");
 
 pub fn handler(
     context: ?*context_manager.Context,
@@ -9,8 +14,48 @@ pub fn handler(
     message: []const u8,
     is_text: bool,
 ) void {
-    _ = context;
     _ = handle;
-    _ = message;
-    _ = is_text;
+
+    const log = std.log.scoped(.websocket_message);
+
+    if (context) |ctx| {
+        if (!is_text) {
+            log.warn("invalid packet received from, {s}", .{ctx.username});
+            return;
+        }
+
+        const isJson = std.json.validate(allocator, message) catch |err| {
+            log.warn("failed to validate JSON: {}", .{err});
+            return;
+        };
+
+        if (!isJson) {
+            log.warn("received malformed packet from {s}, invalid JSON", .{ctx.username});
+            return;
+        }
+
+        const parsedJsonArena = std.json.parseFromSlice(std.json.Value, allocator, message, .{}) catch |err| {
+            log.warn("failed to parse JSON: {}", .{err});
+            return;
+        };
+        defer parsedJsonArena.deinit();
+
+        const parsedJson = parsedJsonArena.value;
+
+        switch (parsedJson) {
+            .object => |obj| {
+                const packetType = json.getValue([]const u8, obj, "type") catch {
+                    return;
+                };
+
+                if (std.mem.eql(u8, packetType, "keyExchange")) {
+                    @import("./handlers/keyExchange.zig").handle_message(ctx, obj) catch |err| {
+                        log.err("failed to handle packet from {s}: {}", .{err});
+                        return;
+                    };
+                }
+            },
+            else => log.warn("malformed packet from {s}, invalid JSON", .{ctx.username}),
+        }
+    }
 }
